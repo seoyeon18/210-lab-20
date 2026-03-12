96b9bb41d9732b15ff606220341aa42c890f4b8a
Your Name <brightsy04@gmail.com>
2026-03-12 15:13:51 -0700

    lab20-4

--- 20.cpp
+++ 20.cpp
@@ -81,13 +81,13 @@ int main()
 
     // creating dynamic array of chair objects
     Chair *collection = new Chair[SIZE];
-    collection[0].setLegs(4);
-    collection[0].setPrices(441.41, 552.52, 663.63);
-    collection[1].setLegs(4);
-    collection[1].setPrices(484.84, 959.59, 868.68);
-    collection[2].setLegs(4);
-    collection[2].setPrices(626.26, 515.15, 757.57);
     for (int i = 0; i < SIZE; i++)
         collection[i].print();
+
+    delete chairPtr;      //free memory
+    chairPtr = nullptr;
+
+    delete[] collection;  //free array memory
+    collection = nullptr;
     return 0;
 }
